02a89b8c249e2811f19379a0d3c3f95872e3a6a6
Takunoko <takunoko@gmail.com>
2021-01-30 17:12:26 +0900

    add say_world function

--- test_sample.py
+++ test_sample.py
@@ -6,3 +6,9 @@ class TestSample(TestCase):
     def test_say_hello(self):
         self.assertEqual(sample.say_hello(), "Hello")
         self.assertNotEqual(sample.say_hello(), "hello")
+
+
+    def test_say_world(self):
+        self.assertEqual(sample.say_world(), "World")
+        self.assertNotEqual(sample.say_world(), "world")
+        self.assertNotEqual(sample.say_world(), "hello")
